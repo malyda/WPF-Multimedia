41938f0ff323b96e4e4a392bed8ed2183830a5f5
David Maly <maly.david.it@gmail.com>
2017-12-12 16:58:00 +0100

    Init
    Complete CJ and Bunny sample

---
+++ Multimedia/Multimedia/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Multimedia
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPlayer.Open(new Uri(@"Media/train.mp3", UriKind.Relative));
+            mediaElement.Source = (new Uri(@"Media/bunny.mp4", UriKind.Relative));
+            mediaElement.LoadedBehavior = MediaState.Manual;
+            mediaElement.ScrubbingEnabled = false;
+
+        }
+
+        private void ButtonPlay_OnClick(object sender, RoutedEventArgs e)
+        {
+           
+            mediaPlayer.Play();
+        }
+
+        private void ButtonStop_OnClick(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void ButtonSetAudio_OnClick(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Open(new Uri(@"Media/train.mp3", UriKind.Relative));
+        }
+
+        private void ButtonSetVideo_OnClick(object sender, RoutedEventArgs e)
+        {
+                mediaElement.Position = new TimeSpan(0, 0, 0);
+                mediaElement.Play();
+        }
+    }
+}
